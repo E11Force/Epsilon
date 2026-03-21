d85581ce45c2abf8a57689fe4e5cd135ebc5d377
Fozik <fozikwashere@gmail.com>
2026-03-21 23:47:07 +0500

    Added Object-Oriented-Programming (konstantin pls fix this shit)

--- Epsilon.cpp
+++ Epsilon.cpp
@@ -4,93 +4,96 @@
 
 using namespace std;
 
-struct BoardState
+class ChessBoard
 {
+private:
 	char Board[8][8];
 	bool MoveTurn = true; // true for white, false for black
 	bool Castling[4] = { true, true, true, true }; // 1,1,1,1 (left part for white, right for black, first for short, second for long)
 	short epRow = -1, epCol = -1; // En passant target square cords (that just jumped over)
-};
 
-vector<int> generateMoves(BoardState& BoardStruct)
-{
-	int knightRow[8] = { -1,1,2,2,1,-1,-2,-2 };
-	int knightCol[8] = { -2,-2,-1,1,2,2,1,-1 };
-
-	for (int i = 0; i < 8; i++)
+public:
+	void boardPrint()
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			if (BoardStruct.Board[i][j] == 'N')
-			{
-				for (int k = 0; k < 8; k++)
-				{
-					int newRow = (i + knightRow[k]);
-					int newCol = (j + knightCol[k]);
+		cout << "   ";
 
-					if ((newRow < 8 && newRow >= 0) && (newCol < 8 && newCol >= 0) && ((islower(BoardStruct.Board[newRow][newCol])) || (BoardStruct.Board[newRow][newCol] == '.')))
-					{
-						cout << newRow << ' ' << newCol << endl;
-						vector<int> Move = { i, j, newRow, newCol };
-					}
-				}
-				cout << endl;
-			}
+		for (int i = 0; i < 8; i++)
+		{
+			cout << i << ' ';
 		}
-	}
-	return vector<int>(0,0);
-}
 
-void boardPrint(BoardState& BoardStruct)
-{
-	cout << "   ";
+		cout << endl << endl;
 
-	for (int i = 0; i < 8; i++)
-	{
-		cout << i << ' ';
-	}
-
-	cout << endl << endl;
-
-	for (int i = 0; i < 8; i++)
-	{
-		cout << i << "  ";
-		for (int j = 0; j < 8; j++)
+		for (int i = 0; i < 8; i++)
 		{
-			cout << BoardStruct.Board[i][j] << " ";
+			cout << i << "  ";
+			for (int j = 0; j < 8; j++)
+			{
+				cout << Board[i][j] << " ";
+			}
+			cout << endl;
 		}
 		cout << endl;
 	}
-	cout << endl;
-}
 
-void setBoardStartPos(BoardState& BoardStruct)
-{
-	for (int i = 0; i < 8; i++) // Empty place setup
+	void setBoardStartPos()
 	{
-		for (int j = 0; j < 8; j++)
+		for (int i = 0; i < 8; i++) // Empty place setup
 		{
-			BoardStruct.Board[i][j] = '.';
+			for (int j = 0; j < 8; j++)
+			{
+				Board[i][j] = '.';
+			}
+		}
+
+		for (int i = 0; i < 8; i++) // Pawns Setup
+		{
+			Board[1][i] = 'p';
+			Board[6][i] = 'P';
+		}
+
+		for (int i = 0; i < 8; i++) // Pieces Setup
+		{
+			Board[0][i] = "rnbqkbnr"[i];
+			Board[7][i] = "RNBQKBNR"[i];
 		}
 	}
 
-	for (int i = 0; i < 8; i++) // Pawns Setup
+	vector<int> generateMoves()
 	{
-		BoardStruct.Board[1][i] = 'p';
-		BoardStruct.Board[6][i] = 'P';
-	}
+		int knightRow[8] = { -1,1,2,2,1,-1,-2,-2 };
+		int knightCol[8] = { -2,-2,-1,1,2,2,1,-1 };
 
-	for (int i = 0; i < 8; i++) // Pieces Setup
-	{
-		BoardStruct.Board[0][i] = "rnbqkbnr"[i];
-		BoardStruct.Board[7][i] = "RNBQKBNR"[i];
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (Board[i][j] == 'N')
+				{
+					for (int k = 0; k < 8; k++)
+					{
+						int newRow = (i + knightRow[k]);
+						int newCol = (j + knightCol[k]);
+
+						if ((newRow < 8 && newRow >= 0) && (newCol < 8 && newCol >= 0) && ((islower(Board[newRow][newCol])) || (Board[newRow][newCol] == '.')))
+						{
+							cout << newRow << ' ' << newCol << endl;
+							vector<int> Move = { i, j, newRow, newCol };
+						}
+					}
+					cout << endl;
+				}
+			}
+		}
+		return vector<int>(0, 0);
 	}
-}
+};
+
 
 int main()
 {
-	BoardState BoardStruct;
-	setBoardStartPos(BoardStruct);
-	boardPrint(BoardStruct);
-	generateMoves(BoardStruct);
+	ChessBoard BoardStruct;
+	BoardStruct.setBoardStartPos();
+	BoardStruct.boardPrint();
+	BoardStruct.generateMoves();
 }

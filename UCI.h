25bfb52b9cc5f54bbe4765b3a69e0bb67b8a4b8c
Fozik <fozikwashere@gmail.com>
2026-04-01 10:17:03 +0500

    Refactoring code.

--- UCI.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-int ParseSquare(string square);
-void ParseMove(string moveString, Board& board);
-void InitUCI(Board& board);
-string SquareToUCI(int index);
